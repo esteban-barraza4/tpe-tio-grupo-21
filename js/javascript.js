9c6852256c72b3ec7a5de18ee1ec8b7464b5f01a
Franco Garrido <francogarrido_@hotmail.com>
2022-10-26 12:52:47 -0300

    cambiando var por let

--- js/javascript.js
+++ js/javascript.js
@@ -1,25 +1,25 @@
 //Declaramos variables
-var operandoa;
-var operandob;
-var operacion;
+let operandoa;
+let operandob;
+let operacion;
 
 function init(){
     //variables
-    var resultado = document.getElementById('resultado');
-    var reset = document.getElementById('reset');
-    var suma = document.getElementById('suma');
-    var resta = document.getElementById('resta');
-    var multiplicacion = document.getElementById('multiplicacion');
-    var division = document.getElementById('division');
-    var igual = document.getElementById('igual');
-    var uno = document.getElementById('uno');
-    var dos = document.getElementById('dos');
-    var tres = document.getElementById('tres');
-    var cuatro = document.getElementById('cuatro');
-    var cinco = document.getElementById('cinco');
-    var seis = document.getElementById('seis');
-    var siete = document.getElementById('siete');
-    var ocho = document.getElementById('ocho');
-    var nueve = document.getElementById('nueve');
-    var cero = document.getElementById('cero');
+    let resultado = document.getElementById('resultado');
+    let reset = document.getElementById('reset');
+    let suma = document.getElementById('suma');
+    let resta = document.getElementById('resta');
+    let multiplicacion = document.getElementById('multiplicacion');
+    let  division = document.getElementById('division');
+    let igual = document.getElementById('igual');
+    let  uno = document.getElementById('uno');
+    let  dos = document.getElementById('dos');
+    let  tres = document.getElementById('tres');
+    let  cuatro = document.getElementById('cuatro');
+    let  cinco = document.getElementById('cinco');
+    let  seis = document.getElementById('seis');
+    let  siete = document.getElementById('siete');
+    let  ocho = document.getElementById('ocho');
+    let  nueve = document.getElementById('nueve');
+    let  cero = document.getElementById('cero');
   }
