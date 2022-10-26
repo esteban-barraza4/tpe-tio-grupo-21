cf28b573a32f5fba2470f75fc12d6c0cd1b40134
Franco Garrido <francogarrido_@hotmail.com>
2022-10-26 15:06:18 -0300

    funcion restaurar

--- js/javascript.js
+++ js/javascript.js
@@ -81,3 +81,13 @@ igual.onclick = function(e){
     operandob = resultado.textContent;
     resolver();
 }
+function limpiar(){
+    resultado.textContent = "";
+  }
+  function resetear(){
+    resultado.textContent = "";
+    operandoa = 0;
+    operandob = 0;
+    operacion = "";
+  }
+  
